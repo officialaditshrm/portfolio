8b293922bcd17b8d9ca8ee813ed044e485416285
officialaditshrm <officialaditshrm@gmail.com>
2025-06-29 15:57:27 +0530

    All files updated

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio",
+  base: "/",
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: 5174,       // You can change the port if needed
+    host: '0.0.0.0',
   },
 })
